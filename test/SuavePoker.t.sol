acae15516b1f3be78528c1a4ce741b94b646d605
Thomas <thomasr928@gmail.com>
2024-05-08 10:56:56 -0500

    Add basic integration tests

--- test/SuavePoker.t.sol
+++ test/SuavePoker.t.sol
@@ -19,7 +19,7 @@ contract SuavePokerTableHarness is SuavePokerTable {
         uint _maxBuyin
     ) SuavePokerTable(_smallBlind, _bigBlind, _minBuyin, _maxBuyin) {}
     // Deploy this contract then call this method to test `myInternalMethod`.
-    function exposed_validTurn(address sender) external returns (bool) {
+    function exposed_validTurn(address sender) external pure returns (bool) {
         return _validTurn(sender);
     }
 
@@ -27,57 +27,21 @@ contract SuavePokerTableHarness is SuavePokerTable {
         return _getPlayer(seat);
     }
 
+    function exposed_getHandState() external returns (HandState memory) {
+        return _getHandState();
+    }
+
     function exposed_transitionHandState(
         HandState memory handStateCurr,
+        PlayerState memory playerStateCurr,
         Action memory action
-    ) external returns (HandState memory) {
-        return _transitionHandState(handStateCurr, action);
+    ) external pure returns (HandState memory, PlayerState memory) {
+        return _transitionHandState(handStateCurr, playerStateCurr, action);
     }
 }
 
 contract TestSuavePoker is Test, SuaveEnabled, ISuavePokerTable {
-    /*
-    event PlayerJoined(address player, uint8 seat, uint stack);
-
-    enum HandStage {
-        SBPost,
-        BBPost,
-        DealHolecards,
-        PreflopBetting,
-        FlopDeal,
-        FlopBetting,
-        TurnDeal,
-        TurnBetting,
-        RiverDeal,
-        RiverBetting,
-        Showdown,
-        Settle
-    }
-    enum ActionType {
-        Bet, // Bet will include Raise - should be total amount bet on that street
-        Fold,
-        Call,
-        Check
-    }
-
-    struct Action {
-        uint256 amount;
-        ActionType act;
-    }
-
-    struct HandState {
-        HandStage handStage;
-        uint8 whoseTurn;
-        Action[] actionList;
-        uint[] boardCards;
-        uint pot;
-        bool handOver;
-        uint facingBet;
-        uint lastRaise;
-    }
-    */
-
-    function testInsertOrder() public {
+    function test_joinTable() public {
         // 1/2 game with buyin of 20-200
 
         uint smallBlind = 1;
@@ -136,7 +100,7 @@ contract TestSuavePoker is Test, SuaveEnabled, ISuavePokerTable {
         assertEq(p1, address(2));
     }
 
-    function testValidTurn() public {
+    function test_validTurn() public {
         // 1/2 game with buyin of 20-200
         SuavePokerTableHarness spt = new SuavePokerTableHarness(1, 2, 20, 200);
         assertFalse(spt.initComplete());
@@ -144,7 +108,7 @@ contract TestSuavePoker is Test, SuaveEnabled, ISuavePokerTable {
         assertTrue(success);
     }
 
-    function testTransitionHandState() public {
+    function test_transitionHandState() public {
         // 1/2 game with buyin of 20-200
         SuavePokerTableHarness spt = new SuavePokerTableHarness(1, 2, 20, 200);
 
@@ -152,26 +116,214 @@ contract TestSuavePoker is Test, SuaveEnabled, ISuavePokerTable {
         Action memory action = Action(100, actType);
 
         HandStage handStage = HandStage.FlopBetting;
-        Action[] memory actionList = new Action[](1);
-        uint[] memory boardCards = new uint[](1);
+        Action memory lastAction = Action(0, ActionType.Bet);
 
         HandState memory handStateCurr = HandState({
             handStage: handStage,
-            whoseTurn: 0,
-            actionList: actionList,
-            boardCards: boardCards,
+            lastAction: lastAction,
             pot: 2,
             handOver: false,
             facingBet: 0,
             lastRaise: 0,
-            stack: 100,
-            inHand: true,
-            playerBetStreet: 0
+            button: 0
         });
 
-        HandState memory handStateNew = spt.exposed_transitionHandState(
+        PlayerState memory playerStateCurr = PlayerState({
+            whoseTurn: 0,
+            stack: 10,
+            inHand: true,
+            playerBetStreet: 20,
+            oppBetStreet: 20
+        });
+
+        HandState memory handStateNew;
+        PlayerState memory playerStateNew;
+
+        (handStateNew, playerStateNew) = spt.exposed_transitionHandState(
             handStateCurr,
+            playerStateCurr,
             action
         );
+    }
+
+    function test_playFullHand() public {
+        // Simulate all the actions to play one full basic hand
+
+        uint smallBlind = 1;
+        uint bigBlind = 2;
+        uint minBuyin = 20;
+        uint maxBuyin = 200;
+        SuavePokerTableHarness spt = new SuavePokerTableHarness(
+            smallBlind,
+            bigBlind,
+            minBuyin,
+            maxBuyin
+        );
+        assertFalse(spt.initComplete());
+
+        // For initialization - we have to initialize both players and table
+        bytes memory o0 = spt.initPlayer(0);
+        address(spt).call(o0);
+
+        bytes memory o1 = spt.initPlayer(1);
+        address(spt).call(o1);
+
+        bytes memory o2 = spt.initTable();
+        address(spt).call(o2);
+
+        assertTrue(spt.initComplete());
+        assertEq(spt.smallBlind(), smallBlind);
+        assertEq(spt.bigBlind(), bigBlind);
+        assertEq(spt.minBuyin(), minBuyin);
+        assertEq(spt.maxBuyin(), maxBuyin);
+
+        uint seed = 123;
+        bytes memory input = abi.encode(seed);
+        ctx.setConfidentialInputs(input);
+
+        // Join as two different players
+        vm.prank(address(1));
+        bytes memory o3 = spt.joinTable(0, 100);
+        vm.expectEmit(true, true, true, true);
+        emit PlayerJoined(address(1), 0, 100);
+        address(spt).call(o3);
+
+        seed = 456;
+        input = abi.encode(seed);
+        ctx.setConfidentialInputs(input);
+
+        vm.prank(address(2));
+        bytes memory o4 = spt.joinTable(1, 100);
+        vm.expectEmit(true, true, true, true);
+        emit PlayerJoined(address(2), 1, 100);
+        address(spt).call(o4);
+
+        // Full action list:
+        // blinds, raise/call, bet/call on each street
+        Action memory a0 = Action(1, ActionType.SBPost);
+        Action memory a1 = Action(2, ActionType.BBPost);
+        Action memory a2 = Action(5, ActionType.Bet);
+        Action memory a3 = Action(0, ActionType.Call);
+
+        bytes memory cb;
+
+        vm.prank(address(1));
+        address(spt).call(spt.takeAction(a0));
+
+        vm.prank(address(2));
+        address(spt).call(spt.takeAction(a1));
+
+        vm.prank(address(1));
+        address(spt).call(spt.takeAction(a2));
+
+        vm.prank(address(2));
+        cb = spt.takeAction(a3);
+        address(spt).call(cb);
+
+        // Should be on flop at this point
+        vm.prank(address(1));
+        cb = spt.takeAction(a2);
+        address(spt).call(cb);
+
+        vm.prank(address(2));
+        cb = spt.takeAction(a3);
+        address(spt).call(cb);
+
+        // Should be on turn at this point
+        vm.prank(address(1));
+        cb = spt.takeAction(a2);
+        address(spt).call(cb);
+
+        vm.prank(address(2));
+        cb = spt.takeAction(a3);
+        address(spt).call(cb);
+
+        // Should be on river at this point
+        vm.prank(address(1));
+        cb = spt.takeAction(a2);
+        address(spt).call(cb);
+
+        vm.prank(address(2));
+        cb = spt.takeAction(a3);
+        address(spt).call(cb);
+
+        // Now showdown, and hand should reset..
+    }
+
+    function test_postBlinds() public {
+        // Simulate all the actions to get to the point of posting blinds
+
+        uint smallBlind = 1;
+        uint bigBlind = 2;
+        uint minBuyin = 20;
+        uint maxBuyin = 200;
+        SuavePokerTableHarness spt = new SuavePokerTableHarness(
+            smallBlind,
+            bigBlind,
+            minBuyin,
+            maxBuyin
+        );
+        assertFalse(spt.initComplete());
+
+        // For initialization - we have to initialize both players and table
+        bytes memory o0 = spt.initPlayer(0);
+        address(spt).call(o0);
+
+        bytes memory o1 = spt.initPlayer(1);
+        address(spt).call(o1);
+
+        bytes memory o2 = spt.initTable();
+        address(spt).call(o2);
+
+        console.log("Init ddone..");
+
+        assertTrue(spt.initComplete());
+        assertEq(spt.smallBlind(), smallBlind);
+        assertEq(spt.bigBlind(), bigBlind);
+        assertEq(spt.minBuyin(), minBuyin);
+        assertEq(spt.maxBuyin(), maxBuyin);
+
+        uint seed = 123;
+        bytes memory input = abi.encode(seed);
+        ctx.setConfidentialInputs(input);
+
+        // Join as two different players
+        vm.prank(address(1));
+        bytes memory o3 = spt.joinTable(0, 100);
+        console.log("p1 joined");
+        vm.expectEmit(true, true, true, true);
+        emit PlayerJoined(address(1), 0, 100);
+        address(spt).call(o3);
+
+        seed = 456;
+        input = abi.encode(seed);
+        ctx.setConfidentialInputs(input);
+
+        vm.prank(address(2));
+        bytes memory o4 = spt.joinTable(1, 100);
+        console.log("p2 joined");
+        vm.expectEmit(true, true, true, true);
+        emit PlayerJoined(address(2), 1, 100);
+        address(spt).call(o4);
+
+        // Full action list:
+        // blinds, raise/call, bet/call on each street
+        Action memory a0 = Action(1, ActionType.SBPost);
+        Action memory a1 = Action(2, ActionType.BBPost);
+
+        console.log("Now takinga ction...");
+
+        vm.prank(address(1));
+        address(spt).call(spt.takeAction(a0));
+
+        // At this stage - handState should be BBPost
+        HandState memory hs = spt.exposed_getHandState();
+        assertTrue(hs.handStage == HandStage.BBPost);
+
+        vm.prank(address(2));
+        address(spt).call(spt.takeAction(a1));
+        // At this stage - handState should be PreflopBet
+        hs = spt.exposed_getHandState();
+        assertTrue(hs.handStage == HandStage.PreflopBetting);
     }
 }
